fa2a64797e4e54a44fdf275bbec536a726179d0e
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-10-12 00:42:40 -0700

    Adds hw1p1.py code

--- hw1/hw1p1.py
+++ hw1/hw1p1.py
@@ -1,46 +1,62 @@
 import snap
+import random
+import numpy as np
+from matplotlib import pyplot
 
-#### PART A ##########################################################################
-# Generate an undirected graph with N = 20000 nodes, using the following iterative
-# procedure:
-#  - Begin with three nodes 0, 1, 2, with edges (0, 1), (1, 2), (2, 0).
-#  - For each additional node n:
-#     + Select an existing node o uniformly at random
-#     + With probability p, add edge (n, o); otherwise, select uniformly at random
-#       one of o’s neighbors, o′, and add edge (n,o′).
-# Plot the normalized degree distributions for p = [0.0,0.2,0.4,0.5,0.8,1.0], all
-# superimposed onto the same log-log graph. Include a legend.
+TOTAL_N_NODES = 2000
+COLORS = ["red", "blue", "green", "purple", "orange", "black"]
+LABELS = ["0.0", "0.2", "0.4", "0.6", "0.8", "1.0"]
 
-def partA():
-  raise Exception
+def set_up_graph():
+  G = snap.TUNGraph.New()
+  for n in [0,1,2]:                 G.AddNode(n)
+  for e in [(0,1), (1,2), (2,0)]:   G.AddEdge(e[0], e[1])
+  return G
 
+def rand_nbr_node(node_index):
+  node        = G.GetNI(node_index)
+  n_neighbors = node.GetDeg()
+  rand_nbr_i  = random.randint(0, n_neighbors - 1)
+  return node.GetNbrNId(rand_nbr_i)
 
-#### PART B ##########################################################################
-# (In the writeup)
+def build_degrees_dict(G):
+  degrees_dict = {}
+  for node_i in range(0, TOTAL_N_NODES):
+    deg_n = G.GetNI(node_i).GetDeg()
+    if deg_n in degrees_dict:   degrees_dict[deg_n] += 1
+    else:                       degrees_dict[deg_n] = 1
+  return degrees_dict
 
+def n_nodes_per_degree(G):
+  degrees_dict = build_degrees_dict(G)
+  n_nodes_per_degree = []
+  for k in degrees_dict.keys():
+    n_nodes_per_degree.append(1.0 * degrees_dict[k] / TOTAL_N_NODES)
+  return degrees_dict.keys(), n_nodes_per_degree
 
-#### PART C ##########################################################################
-# Using the plfit library, compute the estimated xmin and α parameters using the
-# plfit command for the graph described in (a) for p = 0.2. Plfit uses
-# maximum-likelihood estimation (MLE) to compute the parameters. What are the
-# results? What results would you expect, and what factors may contribute to any
-# differences?
+def pyplot_options():
+  pyplot.legend(title="Probability")
+  pyplot.yscale('log')
+  pyplot.xscale('log')
+  pyplot.grid(True)
+  pyplot.xlabel('degree number')
+  pyplot.ylabel('number of nodes')
+  pyplot.legend()
+  pyplot.ylim([0.0001,1])
+  pyplot.show()
 
-def partC():
-  raise Exception
+for i in range(0,6):
+  prob = 0.2 * i
+  G    = set_up_graph()
 
+  for node_i in range(3, TOTAL_N_NODES):
+    random_node_i = random.randint(0, node_i - 1)
+    G.AddNode(node_i)
 
-#### PART D ##########################################################################
-# Generate (without using existing one-line library methods) a Erdos-Renyi random
-# graph with N = 2000 nodes, with probability p = 0.1 of an edge existing between any
-# two nodes, and plot the resulting normalized degree distribution. What type of
-# distribution is the degree distribution? Briefly explain why the distribution differs
-# from the graphs generated in part (a).
-#
-#  1. Graph has n nodes
-#  2. For each pair of possible nodes (a,b) (b, c), (d, e) etc, for example, we do a
-#     coin toss with probability p.
-#  3. If the coin toss is 1, we connect those two edges, if not, we don't.
+    random_prob = random.uniform(0, 1)
+    if random_prob < prob:  G.AddEdge(node_i, random_node_i)
+    else:                   G.AddEdge(node_i, rand_nbr_node(random_node_i))
 
-def partD():
-  raise Exception
+  pyplot.scatter(*n_nodes_per_degree(G), c=COLORS[i], label=LABELS[i], alpha=0.5, edgecolors='none', norm=True)
+
+pyplot_options()
