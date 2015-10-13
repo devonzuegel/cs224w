37c4f12e8c3628e98828b7f17602e9963d32f1fe
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-10-12 22:49:28 -0700

    p1d done

---
+++ hw1/hw1p1d.py
@@ -0,0 +1,24 @@
+from myutil import *
+
+color = 'red'
+label = '0.1'
+
+graph = snap.TUNGraph.New()
+prob = 0.1
+
+for i in range(0, TOTAL_N_NODES):   graph.AddNode(i)
+
+for i in range(0, TOTAL_N_NODES):
+    for j in range(i, TOTAL_N_NODES):
+        if random.uniform(0, 1) < prob:   graph.AddEdge(i, j)
+
+pyplot.scatter(
+    *n_nodes_per_degree(graph),
+    c          = color,
+    label      = label,
+    alpha      = 0.5,
+    edgecolors = 'none',
+    norm       = True
+)
+
+pyplot_options()
