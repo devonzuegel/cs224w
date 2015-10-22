60613e79faf3808e27548af54e5c770b08c9b722
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-10-22 06:10:54 -0700

    Replaces with hash table

--- hw2/hw2p2a.py
+++ hw2/hw2p2a.py
@@ -3,14 +3,14 @@ from collections import defaultdict
 def node(n):    return n[0]
 def degree(n):  return n[1]
 
-adjacencies = defaultdict(list)
+adjacencies = defaultdict(dict)
 with open('data/epinion_signed.txt') as f:
   for line in f:
     tokens = line.split()
     if not tokens[0].isdigit():   continue
     src, dst, sign = tokens
-    adjacencies[src] += [(dst, sign)]
-    adjacencies[dst] += [(src, sign)]
+    adjacencies[src][dst] = sign
+    adjacencies[dst][src] = sign
 
 ordered_nodes = [(a, len(adjacencies[a])) for a in adjacencies]
 ordered_nodes.sort(key = lambda tup: degree(tup), reverse = True)
@@ -18,6 +18,7 @@ ordered_nodes.sort(key = lambda tup: degree(tup), reverse = True)
 A         = defaultdict(list)
 triads    = []
 
+# def get_triads():
 # The following code was inspired by the following paper:
 #   "i11www.iti.uni-karlsruhe.de/extra/publications/sw-fclt-05_wea.pdf".
 for s in ordered_nodes:
